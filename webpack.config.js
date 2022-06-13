6a1790298bfe5b6284b67f42238aea249bcfc76f
unknown <4xv6patelfin@gmail.com>
2022-06-13 17:41:08 -0300

    quase lá 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const webpack= require (webpack)
-   module. exports = {
+   module.exports = {
  mode: "development",
 entry: "./src/principal.js",
 
 output:{
-   filename:"./principal.js", 
-patch__dirname: "./public"
-}
+   filename:"principal.js", 
+   path:_dirname+"/public"
 
 }
+,module:{
+   rules:{
+test:CSSm,
+   use:style-loader,
+   CSS_loader
+   }}}
 
 
